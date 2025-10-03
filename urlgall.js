76f3fb6034f8645727ad9a565ee97202f4a2277c
i82276962 <i82276962@gmail.com>
2025-10-03 10:37:18 +0900

    Update urlgall.js

--- urlgall.js
+++ urlgall.js
@@ -2,7 +2,7 @@
 // @name        유아렐갤 말투 교정기
 // @namespace   http://tampermonkey.net/
 // @version     1.1
-// @description 모바일 지원
+// @description 차단하기 직전에 한번 선처하기 위해 만듬 (설정 기능 추가)
 // @author      urlgall
 // @match       */mini/board/lists/?id=sandboxurl*
 // @match       */mini/board/view/?id=sandboxurl*
@@ -35,8 +35,39 @@
     const L_COUNT = 28;
 
     function fixText(t) {
-        t = t.replace(/[.,]/g, '');
+        const urlR = /(https?:\/\/[^\s<>]+)/g;
+        const tokens = [];
+        let lI = 0;
 
+        // 1. Tokenization: Split text into URL and non-URL segments
+        t.replace(urlR, (match, offset) => {
+            const nUT = t.substring(lI, offset);
+            if (nUT) tokens.push({ v: nUT });
+
+            tokens.push({ v: match, isU: true });
+            lI = offset + match.length;
+            return match;
+        });
+
+        if (lI < t.length) tokens.push({ v: t.substring(lI) });
+
+        let o = '';
+        tokens.forEach(token => {
+            if (token.isU) {
+                // URL은 그대로 유지 (마침표 포함)
+                o += token.v;
+            } else {
+                // 일반 텍스트는 마침표와 쉼표 제거 후 자모 조합
+                const cleanedT = token.v.replace(/[.,]/g, '');
+                o += a(cleanedT);
+            }
+        });
+
+        return o;
+    }
+
+    // Jamo Assembler (기존 fixText에서 클린업 로직 제거 후 'a'로 이름 변경)
+    function a(t) {
         let o = '';
         let c = -1;
         let j = -1;
